3ead908d71f861e3174a730c100dbbf45d507bbc
Jesus Fernandez <jfernandez@multinfmty.com>
2018-02-26 16:07:51 -0600

    Finish v1 with console app to test it

--- pwnedpasswords-validator/Magic.vb
+++ pwnedpasswords-validator/Magic.vb
@@ -1,11 +1,20 @@
 ﻿Module Magic
-    Public Property helper As password_helper
+    Public Property helper As Password_helper = New Password_helper
     Sub Main()
-        Console.WriteLine("Enter a password to check")
+        Console.WriteLine("Please provide a password to check it")
         Dim pwd As String = Console.ReadLine
         If String.IsNullOrWhiteSpace(pwd) Then Console.WriteLine("Please specify a password to check") : Console.ReadKey()
-        Console.WriteLine("Checking password...")
-
+        Console.WriteLine("Let's begin...")
+        Dim hitCount As Integer = helper.GetData(pwd).GetAwaiter.GetResult
+        Select Case hitCount
+            Case -1
+                Console.WriteLine("Troy don't want to help you :( (Something happen)") 'API error
+            Case 0
+                Console.WriteLine("Yay! Your password is not in the internet :D (for now)")
+            Case Else
+                Console.WriteLine($"Oh no, your password has been found {hitCount} {If(hitCount > 1, "times", "time") } on the internet :(")
+        End Select
+        Console.ReadKey()
     End Sub
 
 End Module
--- pwnedpasswords-validator/password_helper.vb
+++ pwnedpasswords-validator/password_helper.vb
@@ -1,13 +1,25 @@
-﻿Imports System.Net.Http
+﻿Imports System.Net
+Imports System.Net.Http
 Imports System.Security.Cryptography
 Imports System.Text
 
-Public Class password_helper
-    Private Const apiUrl As String = "https://api.pwnedpasswords.com/range/"
+Public Class Password_helper
+    Private Const pwd_apiUrl As String = "https://api.pwnedpasswords.com/range/"
     Private Shared ReadOnly httpClient As HttpClient = New HttpClient
 
-    Public Async Function Req(ByVal pwd As String) As Task(Of List(Of String))
-
+    Public Async Function GetData(ByVal pwd As String) As Task(Of Integer)
+        Dim pwd_hash = HashPassword(pwd), pwd_prefix = pwd_hash.Substring(0, 5), pwd_url = $"{pwd_apiUrl}{pwd_prefix}"
+        Using response = Await httpClient.GetAsync(pwd_url)
+            If response.StatusCode = HttpStatusCode.OK Then
+                Dim content = Await response.Content.ReadAsStringAsync
+                Dim lines = content.Split({vbCrLf}, StringSplitOptions.RemoveEmptyEntries)
+                Dim exists = lines.FirstOrDefault(Function(x) $"{pwd_prefix}{x}".StartsWith(pwd_hash))
+                If Not String.IsNullOrWhiteSpace(exists) Then Return Int32.Parse(exists.Split({":"}, StringSplitOptions.RemoveEmptyEntries).Last)
+                Return 0
+            Else
+                Return -1
+            End If
+        End Using
     End Function
 
     Private Function HashPassword(ByVal pwd As String) As String
