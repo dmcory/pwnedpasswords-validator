7d051aab63958ff1c017bdc6de103dae16e346af
Jesus Fernandez <jfernandez@multinfmty.com>
2018-02-26 13:35:09 -0600

    Agregar archivos de proyecto.

---
+++ pwnedpasswords-validator/Magic.vb
@@ -0,0 +1,11 @@
+﻿Module Magic
+    Public Property helper As password_helper
+    Sub Main()
+        Console.WriteLine("Enter a password to check")
+        Dim pwd As String = Console.ReadLine
+        If String.IsNullOrWhiteSpace(pwd) Then Console.WriteLine("Please specify a password to check") : Console.ReadKey()
+        Console.WriteLine("Checking password...")
+
+    End Sub
+
+End Module
---
+++ pwnedpasswords-validator/password_helper.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Net.Http
+Imports System.Security.Cryptography
+Imports System.Text
+
+Public Class password_helper
+    Private Const apiUrl As String = "https://api.pwnedpasswords.com/range/"
+    Private Shared ReadOnly httpClient As HttpClient = New HttpClient
+
+    Public Async Function Req(ByVal pwd As String) As Task(Of List(Of String))
+
+    End Function
+
+    Private Function HashPassword(ByVal pwd As String) As String
+        Using sha As SHA1Managed = New SHA1Managed
+            Dim hash = sha.ComputeHash(Encoding.UTF8.GetBytes(pwd)) : Dim sb = New StringBuilder(hash.Length * 2)
+            For Each b As Byte In hash
+                sb.Append(b.ToString("X2"))
+            Next
+            Return sb.ToString
+        End Using
+    End Function
+End Class
